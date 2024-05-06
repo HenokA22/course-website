9d5a4a6a1c36d3027bf692ffe6722fbc5a05ad82
jimmyle1 <jimmyle1@cs.washington.edu>
2024-05-06 00:20:39 -0700

    fixed linter pt2

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@
 
   /** Function that runs when the page is loaded and sets up other functions. */
   function init() {
-    document.getElementById("login").addEventListener("click", login);
+    id("login").addEventListener("click", login);
   }
 
   /**
@@ -13,7 +13,7 @@
    * @returns {void} - The function does not return anything.
    */
   function login() {
-    console.log('you pressed login button.');
+    id("login").style.display = "none";
   }
 
   /**
@@ -21,25 +21,7 @@
    * @param {string} id - element ID
    * @return {object} DOM object associated with id.
    */
-    function id(id) {
-      return document.getElementById(id);
-    }
-
-  /**
-   * Returns the first element that matches the given CSS selector.
-   * @param {string} query - CSS query selector.
-   * @returns {object[]} array of DOM objects matching the query.
-   */
-  function qs(query) {
-    return document.querySelector(query);
-  }
-
-  /**
-   * Returns the array of elements that match the given CSS selector.
-   * @param {string} query - CSS query selector
-   * @returns {object[]} array of DOM objects matching the query.
-   */
-  function qsa(query) {
-    return document.querySelectorAll(query);
+  function id(id) {
+    return document.getElementById(id);
   }
 })();
