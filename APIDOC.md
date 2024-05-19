f61d351b5e20748be2da2517a48cc2474e8d1901
henok206 <henok206@uw.edu>
2024-05-18 23:04:01 -0700

    Added base endpoints and APIDOC

---
+++ APIDOC.md
@@ -0,0 +1,71 @@
+# *FILL IN NAME* API Documentation
+*Fill in a short description here about the API's purpose.*
+This API allows the clients to send post information such as post name, data, and post message.
+
+## *Fill in Endpoint 1 Title*
+**Request Format:** *Fill in example request format, i.e. the path*
+
+**Request Type:** Get
+
+**Returned Data Format**: Text
+
+**Description:** Returns information pertaining components of a post. Specifically they are name, date, and
+ * message.
+
+**Example Request:** *Fill in example request*
+
+**Example Response:**
+*Fill in example response in the ticks*
+
+```
+
+```
+
+**Error Handling:**
+*Fill in an example of the error handling*
+
+
+## *Fill in Endpoint 2 Title*
+**Request Format:** *Fill in example request format, i.e. the path*
+
+**Request Type:** *Fill in request type*
+
+**Returned Data Format**: JSON
+
+**Description:** *Fill in description*
+
+**Example Request:** *Fill in example request*
+
+**Example Response:**
+*Fill in example response in the {}*
+
+```json
+{
+
+}
+```
+
+**Error Handling:**
+*Fill in an example of the error handling*
+
+
+## *Fill in Endpoint 3 Title*
+**Request Format:** *Fill in example request format, i.e. the path*
+
+**Request Type:** *Fill in request type*
+
+**Returned Data Format**: Plain Text
+
+**Description:** *Fill in description*
+
+**Example Request:** *Fill in example request*
+
+**Example Response:**
+*Fill in example response in the {}*
+
+```
+
+```
+
+**Error Handling:**
+*Fill in an example of the error handling*
---
+++ app.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const multer = require("multer").promises;
+const SUCCESS_CODE = 200;
+const SEVERE_ERROR_CODE = 500;
+const USER_ERROR_CODE = 400;
+const DEFAULT_PORT = 8000;
+
+// for application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); // built-in middleware
+// for application/json
+app.use(express.json()); // built-in middleware
+// for multipart/form-data (required with FormData)
+app.use(multer().none()); // requires the "multer" module
+
+// Feature #3
+app.get("/getItem/:itemName", async function(req, res) {
+  try {
+    let data = await fstat.readFile("data/items.json", "utf-8");
+    data = JSON.parse(data);
+    let itemData = data[req.params.itemName];
+    res.type("json").send(itemData);
+  } catch (error) {
+    res.type("text");
+    if (err.code === "ENOENT") {
+      res.status(SEVERE_ERROR_CODE).send("file does not exist");
+    } else {
+      res.status(SEVERE_ERROR_CODE).send("something went wrong on the server. Please try again.");
+    }
+  }
+});
+
+// Feature #4
+app.get("/checkCourse", async function(req, res) {
+  try {
+
+  } catch (error) {
+
+  }
+});
+
+// Feature #2
+app.get("/login", async function(req, res) {
+  try {
+
+  } catch (error) {
+
+  }
+})
+
+
+
+// tells the code to serve static files in a directory called 'public'
+app.use(express.static('public'));
+
+/*
+ * Allows us to change the port easily by setting an environment
+ * variable, so your app works with our grading software
+ */
+const PORT = process.env.PORT || DEFAULT_PORT;
+app.listen(PORT);
